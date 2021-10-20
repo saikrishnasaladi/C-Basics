d8e9340305cf72f931b523e01122c55084197c2c
saikrishnasaladi <92844074+saikrishnasaladi@users.noreply.github.com>
2021-10-20 12:21:36 +0530

    Add files via upload

---
+++ format 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration 
+	int age;
+    float cgpa;
+	char initial;
+	//Initialization
+	age = 18;
+	cgpa = 7.8;
+	initial = 'S';
+	//Output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+		
+}
