7b6d9242243a0b60ad641d6c2bee1d3eeafd10de
saikrishnasaladi <92844074+saikrishnasaladi@users.noreply.github.com>
2021-11-03 12:04:39 +0530

    Add files via upload

---
+++ aarthematic sums.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int s,a,p;
+	s=5;
+	a = s*s;
+	p = 4*s;
+	printf("area of square with side %d is %d\n",s,a);
+	printf("perimeter of square with side %d is %d",s,p);
+}
