fe5157d9277121fcfcb9899784e6623d088dcdc4
saikrishnasaladi <92844074+saikrishnasaladi@users.noreply.github.com>
2021-11-08 12:07:16 +0530

    Add files via upload

---
+++ comparsion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a = 20;
+	b = 10;
+	printf("a =%d\n",a);
+	printf("b =%d\n",b);
+	printf("%d>%d -->%d\n",a,b,a>b);
+	printf("%d>=%d -->%d\n",a,b,a>=b);
+	printf("%d<%d -->%d\n",a,b,a<b);
+	printf("%d<=%d -->%d\n",a,b,a<=b);
+	printf("%d==%d -->%d\n",a,b,a==b);
+	printf("%d!=%d -->%d\n",a,b,a!=b);
+	
+	
+	
+}
