06d1f42f8a4e762a168b21d142d7274c53ef69af
saikrishnasaladi <92844074+saikrishnasaladi@users.noreply.github.com>
2021-11-09 12:15:58 +0530

    Add files via upload

---
+++ scanf program with variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main ()
+{
+	int a,b,c,d;
+	printf("Enter four numbers");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+ 	printf("product of %d,%d,%dand %d is %d",a,b,c,d,(a*b*c*d));
+ 	
+}
